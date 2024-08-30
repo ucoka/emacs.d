98da56e17fa86a71f02bfc828a63984d30a5a0c8
Yushi Oka <118645045+ucoka@users.noreply.github.com>
2024-08-30 23:44:05 +0900

    Changed ansi-color-black

--- init.el
+++ init.el
@@ -753,6 +753,7 @@ Activate on all buffers." t)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-black ((t (:background "Gray" :foreground "White"))))
  '(ansi-color-blue ((t (:background "blue2" :foreground "deep sky blue"))))
  '(ansi-color-yellow ((t (:background "yellow3" :foreground "Yellow"))))
  '(ediff-current-diff-A ((t (:foreground "firebrick" :background "pale green"))))
